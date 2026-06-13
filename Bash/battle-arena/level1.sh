8e7f938c3399938f2dd8c873b28462a5d0ff3924
AbdallaHayder <86057915+AbdallaHayder@users.noreply.github.com>
2026-06-13 16:54:35 +0300

    Update level1.sh

--- Bash/battle-arena/level1.sh
+++ Bash/battle-arena/level1.sh
@@ -5,12 +5,3 @@ mkdir -p ~/Arena
 touch ~/Arena/{warrior,mage,archer}.txt
 
 ls ~/Arena
-
-# Takeways:
-#    mkdir -p is better than plain mkdir 
-#    This is called making a script idempotent which means:
-#    "Running it multiple times produces the same desired state."
-
-#    ~/Arena/{text1,text1,text3}.txt called a brace expansion.
-#    A more neat representation which Bash expands into:
-#    ~/Arena/text1.txt ~/Arena/text2.txt ~/Arena/text3.txt
